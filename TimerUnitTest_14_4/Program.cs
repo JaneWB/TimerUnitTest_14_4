a10d15cd43c1087166678924862058d41597ada7
JaneWB <brownjannick@att.net>
2018-10-12 14:45:16 -0500

    CG 14-4 TimerTest

--- TimerUnitTest_14_4/Program.cs
+++ TimerUnitTest_14_4/Program.cs
@@ -6,7 +6,18 @@ namespace TimerUnitTest_14_4
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                Console.WriteLine("Enter time as, D:HH:MM:SS");
+                SetTimer timer = new SetTimer(Console.ReadLine());
+                timer.TimerCount();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Incorrect format. Please format as D:H:M:S using colons(:).");
+            }
+
+            Console.ReadLine();
         }
     }
 }
---
+++ TimerUnitTest_14_4/SetTimer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Timers;
+
+namespace TimerUnitTest_14_4
+{
+    class SetTimer
+    {
+        private DateTime startTime;
+        private Timer timer;
+        private TimeSpan span;
+
+        public SetTimer(string time)
+        {
+            span = TimeSpan.Parse(time);
+            timer = new Timer(span.TotalMilliseconds);
+            timer.Elapsed += OnTimedEvent;
+            timer.AutoReset = false;
+            timer.Enabled = true;
+            startTime = DateTime.Now;
+        }
+
+        public void TimerCount()
+        {
+            while (DateTime.Now - startTime < span)
+            {
+                Console.WriteLine("{0:dd\\.hh\\:mm\\:ss}", DateTime.Now.Subtract(startTime));
+                System.Threading.Thread.Sleep(1000);
+            }
+        }
+
+        private void OnTimedEvent(object source, ElapsedEventArgs e)
+        {
+            Console.WriteLine("Times up!");
+        }
+    }
+}
